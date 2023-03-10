c57311f410a8b8e7e058bb9c8203fd8c47b8ec81
hy0yeon <hyoyeon@0420dms.hs.kr>
2023-03-09 19:34:46 -0800

    file2

---
+++ file2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+		int fd;
+		char pathname[] = "temp.txt";
+
+		if((fd=open(pathname, O_RDWR, 0644) == -1)) {
+				printf("Error\n");
+				exit(1);
+		}
+
+		close(fd);
+}
